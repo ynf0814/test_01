6aae7b87f265553b39489c530247a123122c93c0
k yn <ynf0814@naver.com>
2023-10-10 14:19:54 +0900

    장고에서 id로 인수 전달하기...

--- ch99/polls/urls.py
+++ ch99/polls/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from polls import views
 
 urlpatterns=[
-    path('', views.index)
+    path('', views.index),
+    path('creat/', views.creat),
+    path('read/<id>/', views.read),
 ]
--- ch99/polls/views.py
+++ ch99/polls/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse('안녕하세요 polls index 홈페이지에 오신 것을 환영합니다. ')
+
+def creat(request):
+    return HttpResponse('안녕하세요 Creat 홈페이지에 오신 것을 환영합니다. ')
+
+def read(request, id):
+    return HttpResponse('안녕하세요 read '+id+' 홈페이지에 오신 것을 환영합니다. ')
